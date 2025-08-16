2338b1559478ce23eb1d8417c93926d14c414190
gokulakannan19 <goks.cloud19@gmail.com>
2025-08-16 14:35:35 +0000

    user and auth created

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from .database import Base, engine
+from .routers import users
 
 
 Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(title="Petstagram", version="1.0.0")
+
+app.include_router(users.router)
 
 
 @app.get("/")
---
+++ app/routers/users.py
@@ -0,0 +1,52 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
+from sqlalchemy.orm import Session
+from ..models import User
+from ..schemas import UserCreate, UserOut, Token
+from ..database import SessionLocal
+from ..auth import get_password_hash, verify_password, create_access_token
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"]
+)
+
+
+def get_db():
+    """Dependency to get the database session."""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/signup", response_model=UserOut, status_code=status.HTTP_201_CREATED)
+def create_user(db: db_dependency, user: UserCreate):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email Already Registered")
+    hashed_password = get_password_hash(user.password)
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hashed_password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+    
+
+@router.post("/login", response_model=Token)
+async def login(db: db_dependency, user: UserCreate):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
+    access_token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
